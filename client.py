776d83f72427b69be724c40ec463ff68bd19e5e2
frodo <dvoretsky.boriss@gmail.com>
2019-07-14 23:01:18 +0300

    hey :)

--- client.py
+++ client.py
@@ -4,8 +4,6 @@ import sys
 socket = socket.socket()
 socket.connect(('localhost', 62000))
 
-socket.send("Hi serve".encode())
-
 data = socket.recv(1024)
 
-print(data.decode('UTF-8'))
+print(data.decode())
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
+import os
 import sys
+
+map = open('MAP_1.txt', 'r')
 
 socket = socket.socket()
 socket.bind(('localhost', 62000))
@@ -7,12 +10,7 @@ socket.listen(1)
 
 new_sock, new_addr = socket.accept()
 
-message_from_client = new_sock.recv(1024)
-
-if message_from_client.decode('UTF-8') == "Hi server":
-    new_sock.send("It's 200OK".encode('UTF-8'))
-else:
-    new_sock.send("404 Error".encode('UTF-8'))
+new_sock.send(map.read().encode())
 
 
 new_sock.close()
