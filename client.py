4b3138613f890a69338a115b954366563c7635c9
frodo <dvoretsky.boriss@gmail.com>
2019-07-17 14:39:50 +0300

    correct version

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import socket
 import sys
+import os
+
+clear = lambda: os.system('cls')
 
 socket = socket.socket()
 socket.connect(('localhost', 62000))
@@ -7,3 +10,18 @@ socket.connect(('localhost', 62000))
 data = socket.recv(1024)
 
 print(data.decode())
+
+while True:
+    command = input()
+    if command.upper()[0] in ['W', 'A', 'S', 'D']:
+        socket.send(command.upper()[0].encode())
+
+        clear()
+
+        data = socket.recv(1024)
+
+        print(data.decode())
+
+    elif command == 'exit()':
+        socket.send(command.encode())
+        break
--- server.py
+++ server.py
@@ -4,13 +4,79 @@ import sys
 
 map = open('MAP_1.txt', 'r')
 
+new_map = []
+
+for line in map:
+    for x in line:
+        if x == '\n':
+            continue
+        new_map += x
+
+pos = 209
+
+for i in range(len(new_map)):
+    if new_map[i] == 'A':
+        pos = i
+
+def convert_to_string(map):
+    string = ''
+    for i in range(len(map)):
+        string += map[i]
+        if (i + 1) % 20 == 0:
+            string += '\n'
+
+    return string
+
+
 socket = socket.socket()
 socket.bind(('localhost', 62000))
 socket.listen(1)
 
 new_sock, new_addr = socket.accept()
 
-new_sock.send(map.read().encode())
+new_sock.send(convert_to_string(new_map).encode())
+
+while True:
+    data = new_sock.recv(1024)
+
+    if data.decode() == 'exit()':
+        new_sock.close()
+        break
+
+    elif data.decode() == 'W':
+        if new_map[pos - 20] == ' ':
+            [new_map[pos], new_map[pos - 20]] = [new_map[pos - 20], new_map[pos]]
+            new_sock.send(convert_to_string(new_map).encode())
+            pos -= 20
+        else:
+            new_sock.send(convert_to_string(new_map).encode())
+            new_sock.send("You can't go there".encode())
+
+    elif data.decode() == 'A':
+        if new_map[pos - 1] == ' ':
+            [new_map[pos], new_map[pos - 1]] = [new_map[pos - 1], new_map[pos]]
+            new_sock.send(convert_to_string(new_map).encode())
+            pos -= 1
+        else:
+            new_sock.send(convert_to_string(new_map).encode())
+            new_sock.send("You can't go there".encode())
+
+    elif data.decode() == 'S':
+        if new_map[pos + 20] == ' ':
+            [new_map[pos], new_map[pos + 20]] = [new_map[pos + 20], new_map[pos]]
+            new_sock.send(convert_to_string(new_map).encode())
+            pos += 20
+        else:
+            new_sock.send(convert_to_string(new_map).encode())
+            new_sock.send("You can't go there".encode())
+
+    elif data.decode() == 'D':
+        if new_map[pos + 1] == ' ':
+            [new_map[pos], new_map[pos + 1]] = [new_map[pos + 1], new_map[pos]]
+            new_sock.send(convert_to_string(new_map).encode())
+            pos += 1
+        else:
+            new_sock.send(convert_to_string(new_map).encode())
+            new_sock.send("You can't go there".encode())
 
 
-new_sock.close()
